f2a3c625757f8c7d26e910e93788ad0007d66a5f
Fernando Huerta <fernando.huerta984@gmail.com>
2025-06-04 21:01:59 -0300

    Correccion Estructura Back

---
+++ BackEnd/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoGastosAPI.Models;
+
+namespace ProyectoGastosAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Gasto> Gastos { get; set; }
+    }
+}
